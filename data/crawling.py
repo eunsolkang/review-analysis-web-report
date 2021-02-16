2ccc2150b7ed20dffd82e0a951f29beebda03352
citron0137 <citron0137@gmail.com>
2021-02-15 17:16:07 -0800

    :floppy_disk: data: Add but token and label again(#6)

--- data/crawling.py
+++ data/crawling.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException 
 import csv
-
+import re
 class AmazonProduct:
     
     def __init__(self, url):
@@ -18,7 +18,7 @@ class AmazonProduct:
             print("plz set driver first")
             return
         self.webDriver.get(self.url)
-        reviewpage = self.webDriver.find_element_by_css_selector('#reviews-medley-footer > div.a-row.a-spacing-medium > a') 
+        reviewpage = self.webDriver.find_element_by_css_selector('#reviews-medley-footer > div.a-row.a-spacing-medium > a') #TODO change sometime 
         reviewpage.click()
         while True:
             elems = self.webDriver.find_elements_by_css_selector("div[data-hook='review']")
@@ -66,11 +66,29 @@ class AmazonProduct:
             reviewStr = reviewStr.replace('。','\n')
 
             strs = reviewStr.split('\n')
+           
+            resStrs=[]
+            but_re = re.compile("(\\b:?but\W|however\W|while\W\\b)")
+            for st in strs:
+                lower_st = st.lower()
+                but_pt = but_re.split(lower_st)
+                keep_st=""
+                for i in but_pt:
+                    if("but" in i or "however" in i or "while" in i):
+                        keep_st = i
+                    else:
+                        res_st = keep_st+i
+                        res_st=res_st.strip()
+                        if(len(res_st) == 0 or len(res_st) == 1):
+                            continue
+                        resStrs.append(res_st)
+                        keep_st = ""
+            
 
-            strs = [v.strip() for v in strs]    
-            strs = [v for v in strs if len(v) != 0]
-            strs = [v for v in strs if len(v) != 1]
-            review['reviewStrs'] = strs
+            #strs = [v.strip() for v in strs]    
+            #strs = [v for v in strs if len(v) != 0]
+            #strs = [v for v in strs if len(v) != 1]
+            review['reviewStrs'] = resStrs
         return
     
     def str2csv(self, filePath):
@@ -99,12 +117,17 @@ def getHeadlessWebDriver():
 
 if __name__ == "__main__":
     
-    link1 = "https://www.amazon.com/Van-Heusen-Pinpoint-Regular-Stripe/dp/B017EUJCJS/ref=sr_1_6?dchild=1&keywords=dress+shirts&qid=1613024659&sr=8-6"
-    link2 = "https://www.amazon.com/Van-Heusen-Regular-Stretch-X-Large/dp/B07RFN9YXW/ref=sr_1_9?dchild=1&keywords=dress+shirts&qid=1613024659&sr=8-9"
-    link3 = "https://www.amazon.com/Amazon-Essentials-Regular-Fit-Long-Sleeve-Sleeve/dp/B06XX648KY/ref=sr_1_11?dchild=1&keywords=dress+shirts&qid=1613024659&sr=8-11"
+    #link1 = "https://www.amazon.com/Van-Heusen-Pinpoint-Regular-Stripe/dp/B017EUJCJS/ref=sr_1_6?dchild=1&keywords=dress+shirts&qid=1613024659&sr=8-6"
+    #link2 = "https://www.amazon.com/Van-Heusen-Regular-Stretch-X-Large/dp/B07RFN9YXW/ref=sr_1_9?dchild=1&keywords=dress+shirts&qid=1613024659&sr=8-9"
+    #link3 = "https://www.amazon.com/Amazon-Essentials-Regular-Fit-Long-Sleeve-Sleeve/dp/B06XX648KY/ref=sr_1_11?dchild=1&keywords=dress+shirts&qid=1613024659&sr=8-11"
 
-    driver = getWebDriver()
+    link4 = "https://www.amazon.com/Van-Heusen-Poplin-Regular-Collar/dp/B009ESZFM2/ref=sr_1_5?crid=3UQKALUSIW2YI&dchild=1&keywords=dress+shirts+for+men&qid=1613098910&sprefix=dress+sh%2Caps%2C365&sr=8-5"
+    link5 = "https://www.amazon.com/Van-Heusen-Poplin-Fitted-Collar/dp/B00C86E80W/ref=sr_1_6?crid=3UQKALUSIW2YI&dchild=1&keywords=dress+shirts+for+men&qid=1613099257&sprefix=dress+sh%2Caps%2C365&sr=8-6"
+    link6 = "https://www.amazon.com/Coofandy-Casual-Button-Shirts-Medium/dp/B075WQND21/ref=sr_1_7?crid=3UQKALUSIW2YI&dchild=1&keywords=dress+shirts+for+men&qid=1613099257&sprefix=dress+sh%2Caps%2C365&sr=8-7"
 
+    #driver = getWebDriver()
+
+    '''
     shirt1 = AmazonProduct(link1)
     shirt1.setWebDriver(driver)
     shirt1.crawlReviews()
@@ -125,3 +148,31 @@ if __name__ == "__main__":
     shirt3.reviews2csv("./reviews3.csv")
     shirt3.review2str()
     shirt3.str2csv("./review_seperated_by_sentences3.csv")
+
+    shirt4 = AmazonProduct(link4)
+    shirt4.setWebDriver(driver)
+    shirt4.crawlReviews()
+    shirt4.reviews2csv("./reviews4.csv")
+    shirt4.review2str()
+    shirt4.str2csv("./review_seperated_by_sentences4.csv")
+    
+    shirt5 = AmazonProduct(link5)
+    shirt5.setWebDriver(driver)
+    shirt5.crawlReviews()
+    shirt5.reviews2csv("./reviews5.csv")
+    shirt5.review2str()
+    shirt5.str2csv("./review_seperated_by_sentences5.csv")
+    
+    shirt6 = AmazonProduct(link6)
+    shirt6.setWebDriver(driver)
+    shirt6.crawlReviews()
+    shirt6.reviews2csv("./reviews6.csv")
+    shirt6.review2str()
+    shirt6.str2csv("./review_seperated_by_sentences6.csv")
+    '''
+
+    shirt = AmazonProduct("#")
+    shirt.readReviewsFromCsv("tmpDatas/reviews1.csv")
+    shirt.review2str()
+    shirt.str2csv("./test.csv")
+
