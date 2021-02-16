f51477575d62d4a6e6b251445831ba3f02d0c2fa
eunsolkang <eunsol2953@gmail.com>
2021-02-16 22:56:33 +0900

    🔨features: create Page Not Found Page #4

---
+++ frontend/src/components/common/NotFound.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette'
+const NotFoundBlock = styled.div`
+    display : flex;
+    flex-direction: column;
+    justify-content : center;
+    align-items : center;
+    height: 100vh;
+    h1{
+        color : white;
+    }
+    h3{
+        color : white;
+    }
+    background : ${palette.main[0]};
+
+`;
+
+const NotFound = () => {
+    return (
+        <NotFoundBlock>
+            <h1>404 NotFound</h1>
+            <h3>없는 페이지 입니다!</h3>
+            
+        </NotFoundBlock>
+    )
+}
+
+export default NotFound;
