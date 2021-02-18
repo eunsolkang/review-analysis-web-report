06fde5d3840e059a20af66d1000d5b6f417918ce
eunsolkang <eunsol2953@gmail.com>
2021-02-19 02:56:44 +0900

    🔨 features: Create Word Cloud #4

--- frontend/src/components/Review.jsx
+++ frontend/src/components/Review.jsx
@@ -9,10 +9,11 @@ const ReviewBlock = styled.div`
     section{
       display: flex;
       flex-direction: row;
+      justify-content: space-between;
       margin-top: 1rem;
       border-radius: 10px;
       background: white;
-      padding: 1rem;
+      padding: 2rem;
       article{
         flex:1;
         display: flex;
@@ -21,6 +22,12 @@ const ReviewBlock = styled.div`
         align-items: center;
         h4{
           text-align: center;
+        }
+        img{
+          margin-right: 1rem;
+          margin-left: 1rem;
+          width: 90%;
+          box-shadow: 0 10px 40px 0 rgba(195, 208, 226, 0.7);
         }
       }
     }
@@ -34,23 +41,22 @@ const Review = () => {
             </h3>
             <section>
                 <article>
-                    <Plot
-                        data={[
-                            {
-                                type: 'bar',
-                                x: [20, 14, 23],
-                                y: ['great', 'normal', 'bad'],
-                                orientation: 'h'
-                            },
-                        ]}
-                        layout={ {width: 500, height: 400, title: 'Pos / Neg Review'} }
-                    />
+                    <h4>
+                        All Word
+                    </h4>
+                    <img src={'/img/wordcloud1.png'}/>
                 </article>
                 <article>
                     <h4>
-                        Word Cloud
+                        Negative Word
                     </h4>
-                    <img src={'/img/wordcloud.png'}/>
+                    <img src={'/img/wordcloud2.png'}/>
+                </article>
+                <article>
+                    <h4>
+                        Positive Word
+                    </h4>
+                    <img src={'/img/wordcloud3.png'}/>
                 </article>
             </section>
         </ReviewBlock>
