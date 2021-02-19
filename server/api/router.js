55413e64672d720867d61137ee14f323a5e3c8d5
citron0137 <citron0137@gmail.com>
2021-02-19 04:45:11 -0800

    :sparkles: features: Add mockup data(#23)

--- server/api/router.js
+++ server/api/router.js
@@ -1,4 +1,13 @@
 const router = require('express').Router()
 
+const product = require('./product').product
+const reviewAnalysis = require('./reviewAnalysis').reviewAnalysis
+const trendAnalysis = require('./trendAnalysis').trendAnalysis
+
+router.post('/product', product)
+router.get('/reviewAnalysis', reviewAnalysis)
+router.get('/trendAnalysis', trendAnalysis)
+
+
 module.exports = router
 
